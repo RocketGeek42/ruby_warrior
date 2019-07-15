eab7872780f6136b8df16404ce995b1ad1e265a3
jmyork <jen@yorkitconsulting.com>
2019-07-15 13:00:18 -0500

    Update level8.rb
    Added code for level 8

--- Beginner/level8.rb
+++ Beginner/level8.rb
@@ -1,2 +1,28 @@
 # Hear mumbling of wizards. Beware of their deadly wands!  Good thing you found a bow.
 # Use warrior.look to determine surroundings, and warrior.shoot! to fire an arrow.
+
+class Player
+
+    def play_turn(warrior)
+        if look_foward(warrior) == "captive"
+            if (warrior.feel.captive?)
+                warrior.rescue!
+            else
+                warrior.walk!
+            end
+        elsif look_foward(warrior) == "enemy"
+            warrior.shoot!
+        else
+            warrior.walk!
+        end
+    end
+
+    def look_foward(warrior)
+        if (warrior.look.any? { |square| square.captive?})
+            return "captive"
+        elsif (warrior.look.any? { |square| square.enemy?})
+            return "enemy"
+        end
+    end
+
+end
