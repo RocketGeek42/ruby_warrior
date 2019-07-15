432ebc927eef161d32f565589ed7727d9947cd57
jmyork <jen@yorkitconsulting.com>
2019-07-15 12:41:06 -0500

    Create level1.rb
    Create Beginner and Intermediate folders, added code for Level 1 Beginner

---
+++ Beginner/level1.rb
@@ -0,0 +1,8 @@
+#You see before yourself a long hallway with stairs at the end.  There is nothing in the way.
+# Call warrior.walk! to walk forward in the Player 'play_turn' method.
+# warrior.walk! move in the given direction (forward by default)
+class Player
+    def play_turn(warrior)
+        warrior.walk!
+    end
+end
