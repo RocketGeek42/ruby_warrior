c43080c0690c4c1538e650e5a604b2779e903acb
jmyork <jen@yorkitconsulting.com>
2019-07-19 11:40:09 -0500

    Update level3.rb

--- Intermediate/level3.rb
+++ Intermediate/level3.rb
@@ -2,3 +2,15 @@
 # Call warrior.bind!(direction) to bind an enemy to keep him from attacking.
 # Bound enemies look like captives.
 
+class Player
+
+    def initialize()
+        @health = 0
+        @direction = [:forward, :backward, :right, :left]
+    end
+
+    def play_turn(warrior)
+        # code goes here
+    end
+
+    
